b5ec6bfc97dbd8ace2a725bac85807126997c1bf
FullStackJiung <stacksjiung@gmail.com>
2023-09-26 11:08:15 +0900

    quiz demo

--- biblequiz/lib/models.dart
+++ biblequiz/lib/models.dart
@@ -1,3 +1,5 @@
+import 'package:biblequiz/list.dart';
+
 class Quiz {
   final String question;
   final List<String> options;
@@ -14,10 +16,15 @@ class Quiz {
 
 final List<Quiz> quizzes = [
   Quiz(
-    question: '창세기에서 아담의 부인의 이름은 무엇인가요?',
-    options: ['사라', '레아', '라헬', '하와'],
-    answerIndex: 3,
-    rewardPageContent: '하나님께서는 아담의 갈빗대로 하와를 창조하셨다...',
+    question: ' 태초에 하나님이 창조하신 것은?',
+    options: ['물고기', '천사', '천지', '산'],
+    answerIndex: 2,
+    rewardPageContent: list[0],
+  ),Quiz(
+    question: '땅의 초기 상태는 혼돈하고 공허하였다',
+    options: ['O', 'X'],
+    answerIndex: 0,
+    rewardPageContent: list[1]+list[2]+list[3],
   ),
   // 다른 퀴즈들 추가
 ];
