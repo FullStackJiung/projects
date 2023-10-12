87d38f3476f7c499cb3d19063caf8411851b18ca
FullStackJiung <stacksjiung@gmail.com>
2023-10-12 11:56:00 +0900

    bottom nav:

--- uistudy/lib/pages/home_page.dart
+++ uistudy/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'dart:ffi';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:uistudy/util/my_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,69 +13,114 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //pagecontroller
+
+  final _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.pink,
+        child: Icon(Icons.monetization_on),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.grey[200],
+        child: Padding(
+          padding: const EdgeInsets.only(top:8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+            IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.book,size: 32, color: Colors.pink[200],),),
+            IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.pencil_outline,size: 35, color: Colors.pink[200]),)
+          ],),
+        ),
+      ),
       body: SafeArea(
-        child: Column(children: [
-          //appbar
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                 Row(
-                  children: [
-                    Text(
-                      'My',
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
+        child: Column(
+          children: [
+            //appbar
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'My',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    Text(
-                      ' Cards',
-                      style: TextStyle(fontSize: 28),
-                    ),
-                  ],
-                ),
-                Container(
-                  padding: EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[400],
-                    shape: BoxShape.circle,
+                      Text(
+                        ' Cards',
+                        style: TextStyle(fontSize: 28),
+                      ),
+                    ],
                   ),
-                  child: Icon(Icons.add),
-                ),
-              ],
+                  Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.add),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-
-          //cards
-          Container(
-            height: 150,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                MyCard(number:'3:4',title:'ssdfwers',color: Colors.lightBlue,),
-                MyCard(number:'55:4',title:'ssdfwers',color: Colors.lightGreen,),
-                MyCard(number:'ssssss',title:'ssdfwers',color: Colors.limeAccent,),
-          
-              ],
+            const SizedBox(
+              height: 25,
             ),
-          )
 
-          //3 buttons
+            //cards
+            Container(
+              height: 150,
+              child: PageView(
+                scrollDirection: Axis.horizontal,
+                controller: _controller,
+                children: [
+                  MyCard(
+                    number: '3:4',
+                    title: 'ssdfwers',
+                    color: Colors.lightBlue,
+                  ),
+                  MyCard(
+                    number: '55:4',
+                    title: 'ssdfwers',
+                    color: Colors.lightGreen,
+                  ),
+                  MyCard(
+                    number: 'ssssss',
+                    title: 'ssdfwers',
+                    color: Colors.limeAccent,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            SmoothPageIndicator(
+              controller: _controller,
+              count: 3,
+              effect: ExpandingDotsEffect(
+                activeDotColor: Colors.grey.shade800,
+              ),
+            )
 
-          //column
-        ]),
+            //3 buttons
+
+            //column
+          ],
+        ),
       ),
     );
   }
